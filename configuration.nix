970a7f44b4e9afdd04a34ad6fdbdd9a6f2bf9e94
BlueDuDe04 <Bennettmason04@gmail.com>
2023-09-08 09:43:17 -0700

    commit message

---
+++ configuration.nix
@@ -0,0 +1,275 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+      ./nixos-modules.nix
+    ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+
+  networking.hostName = "NixOS"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Vancouver";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_CA.UTF-8";
+  
+  security.rtkit.enable = true;
+  security.polkit.enable = true;
+
+  programs.river = {
+    enable = true;
+  };
+
+  services.dbus.enable = true;
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    # gtk portal needed to make gtk apps happy
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
+
+
+  services = {
+    xserver = {
+
+      # xkbOptions = "compose:ralt";
+      # layout = "us";
+      # xkbVariant = ",colemak_dh";
+      # xkbOptions = "grp:alt_space_toggle";
+
+      libinput = {
+        enable = true;
+        touchpad.naturalScrolling = true;
+      };
+
+      # displayManager.startx.enable = true;
+      # enable = true;
+      # displayManager = {
+      #   lightdm.enable = true;
+      #   defaultSession = "none+bspwm";
+      # };
+      # windowManager.bspwm.enable = true;
+    };
+
+    greetd ={
+      enable = true;
+      settings = {
+        default_session = {
+          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd river";
+          user = "greeter";
+       };
+      };
+    };
+    
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+    };
+    
+    kmonad = {
+      enable = true;
+      package = (import ./kmonad.nix) pkgs;
+      keyboards = {
+        "laptop" = {
+          defcfg = {
+            enable = true;
+            compose.key = null;
+            fallthrough = false;
+          };
+
+          device = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
+          config = builtins.readFile ./colemak-dh-wide.kbd;
+        };
+      };
+    };
+  };
+  
+  virtualisation.docker.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.bluedude = {
+    isNormalUser = true;
+    # description = "";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    packages = with pkgs; [];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
+  
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    # CLI
+    git
+    lazygit
+    wget
+    zip
+    unzip
+    mpv
+    killall
+    htop
+    openvpn
+    ranger
+    evtest
+    pamixer
+    brightnessctl
+    imagemagick
+
+    # Editors
+    neovim
+    lapce
+    helix
+
+    # Wayland 
+    wayland
+    xwayland
+    xdg-desktop-portal-wlr
+
+    eww-wayland
+    wlr-randr
+    waybar
+    wofi
+    swww
+    grim
+    slurp
+    swappy
+    wl-clipboard
+    wf-recorder
+
+    swayfx
+    swaybg
+    swaylock
+    swayidle
+
+    river
+    river-luatile
+    river-tag-overlay
+
+    # X11
+    #rofi
+    #picom
+    #polybar
+    #bspwm
+    #sxhkd
+    #nitrogen
+
+    # Apps
+    qutebrowser
+    python311Packages.adblock
+
+    firefox
+    librewolf
+    wezterm
+    pavucontrol
+    dbeaver
+    obs-studio
+    freetube
+    slack
+    discord
+
+    # Shells
+    fish
+    nushell
+    starship
+    zoxide
+
+    # Langs
+    zig
+    zls
+
+    gcc9
+    gnumake
+    clang
+    clang-tools
+    valgrind
+
+    lua
+    lua-language-server
+
+    typescript
+    nodePackages.typescript-language-server
+
+    rustc
+    cargo
+    rust-analyzer
+
+    go
+    gopls
+
+    rnix-lsp
+
+    # Other
+    docker
+    docker-compose
+    seatd
+    polkit
+    mesa
+    glxinfo
+    openssl
+    
+    # (builtins.getFlake "path:/home/bluedude/Documents/code/dwl").packages.x86_64-linux.default
+  ];
+
+  fonts.packages = with pkgs; [
+    (nerdfonts.override { fonts = ["FiraCode"]; })
+  ];
+
+  nix.settings.auto-optimise-store = true;
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.11"; # Did you read the comment?
+}
---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "NixOS Flake";
+
+  inputs = {
+    nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
+  };
+
+  outputs = { self, nixpkgs }: 
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+    	inherit system;
+
+	config = {
+	   allowUnfree = true;
+	};
+    };
+  in {
+    nixosConfigurations = {
+
+      myNixOS = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit system; };
+
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+  };
+}
