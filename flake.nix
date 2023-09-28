30840a1d38413d0f6a0efbbd80b7c5699c00a936
BlueDuDe04 <Bennettmason04@gmail.com>
2023-09-27 22:43:30 -0700

    Flake Clean Up

--- flake.nix
+++ flake.nix
@@ -2,27 +2,23 @@
   description = "NixOS Flake";
 
   inputs = {
-    nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     stylix.url = "github:danth/stylix";
 
     kmonad.url = "github:kmonad/kmonad?dir=nix";
 
-    transparent-nvim = {
-      url = "github:xiyaowong/transparent.nvim";
-      flake = false;
-    };
+    transparent-nvim.url = "github:xiyaowong/transparent.nvim";
+    transparent-nvim.flake = false;
   };
 
   outputs = { self, nixpkgs, home-manager, stylix, ... }@inputs: 
   let
     system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
+    # pkgs = import nixpkgs { inherit system; };
   in {
     nixosConfigurations = {
       NixOS = nixpkgs.lib.nixosSystem {
