a95e621bd4f78b7a1c499b5a22a235de1a20684f
BlueDuDe04 <Bennettmason04@gmail.com>
2023-11-18 10:38:18 -0800

    Hyprland v0.31.0

--- flake.nix
+++ flake.nix
@@ -15,8 +15,8 @@
 
     xremap.url = "github:xremap/nix-flake";
 
-    hyprland.url = "github:hyprwm/Hyprland?ref=v0.32.0";
-    hy3.url = "github:outfoxxed/hy3?ref=hl0.32.0";
+    hyprland.url = "github:hyprwm/Hyprland?ref=v0.31.0";
+    hy3.url = "github:outfoxxed/hy3?ref=hl0.31.0";
     hy3.inputs.hyprland.follows = "hyprland";
 
     firefox-addons.url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
