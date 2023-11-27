b46e5028fc0e8ff753e704127b381050bff35e14
BlueDuDe04 <Bennettmason04@gmail.com>
2023-11-26 22:19:04 -0800

    Desktop Workspaces

--- flake.nix
+++ flake.nix
@@ -78,10 +78,23 @@
                   ];
 
                   extraConfig = (import ./home/hypr.nix) inputs ''
-                    monitor=DP-1,2560x1440@59.95100,0x0,1
+                    monitor=DP-3,2560x1440@59.95100,0x0,1
                     monitor=HDMI-A-1,2560x1440@59.95100,2560x-545,1,transform,3
 
-                    exec-once = swaybg -o DP-1 -m stretch -i ~/git/NixOS/colorful-sky.jpg
+                    exec-once = swaybg -o DP-3 -m stretch -i ~/git/NixOS/colorful-sky.jpg
+                    exec-once = swaybg -o HDMI-A-1 -m fill -i ~/git/NixOS/Colorful-Sky-Vertical.jpg
+
+                    workspace = DP-3, 9
+                    workspace = DP-3, 5
+                    workspace = DP-3, 1
+                    workspace = DP-3, 3
+                    workspace = DP-3, 7
+
+                    workspace = HDMI-A-1, 6
+                    workspace = HDMI-A-1, 2
+                    workspace = HDMI-A-1, 0
+                    workspace = HDMI-A-1, 4
+                    workspace = HDMI-A-1, 8
                   '';
                 };
               }
