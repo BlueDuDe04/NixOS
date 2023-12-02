4b8efa31f892ba9f0e454801c888b9f44bb5d895
BlueDuDe04 <Bennettmason04@gmail.com>
2023-12-02 15:05:56 -0800

    more tests

--- os/server.nix
+++ os/server.nix
@@ -91,6 +91,15 @@
 
     jellyfin.enable = true;
   };
+
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 80 443 ];
+    allowedUDPPortRanges = [
+      { from = 4000; to = 4007; }
+      { from = 8000; to = 8010; }
+    ];
+  };
   
   virtualisation.docker.enable = true;
 
