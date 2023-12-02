7cf0540e64f2a1c39d7b38fd45c8ecb21017f116
BlueDuDe04 <Bennettmason04@gmail.com>
2023-12-02 15:39:02 -0800

    test nginx

--- os/server.nix
+++ os/server.nix
@@ -71,14 +71,14 @@
       virtualHosts."192.168.0.40" =  {
         enableACME = false;
         forceSSL = false;
-        # locations = {
-        #   "/" = {
-        #     proxyPass = "http://127.0.0.1:8384";
-        #     proxyWebsockets = true;
-        #   };
-        #   # "/".proxyPass = "http://127.0.0.1:8096";
-        #   # "/gameyfin".proxyPass = "http://127.0.0.1:xxxx";
-        # };
+        locations = {
+          "/" = {
+            proxyPass = "http://127.0.0.1:8384";
+            proxyWebsockets = true;
+          };
+          # "/".proxyPass = "http://127.0.0.1:8096";
+          # "/gameyfin".proxyPass = "http://127.0.0.1:xxxx";
+        };
       };
     };
 
