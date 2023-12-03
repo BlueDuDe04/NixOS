e738e4665c01a8eb4cd0f63051baccfe421d3b66
BlueDuDe04 <Bennettmason04@gmail.com>
2023-12-02 18:16:59 -0800

    test :xxxx

--- os/server.nix
+++ os/server.nix
@@ -68,9 +68,9 @@
       recommendedProxySettings = true;
       recommendedTlsSettings = true;
 
-      virtualHosts."192.168.0.40:8384" =  {
+      virtualHosts."192.168.0.40" =  {
         locations = {
-          "/" = {
+          ":8384/" = {
             proxyPass = "http://127.0.0.1:8384";
             # proxyPass = "http://127.0.0.1:8096";
             proxyWebsockets = true;
