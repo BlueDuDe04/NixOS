4dbe76b5fb11a716e45970e6446669e1e801bd60
BlueDuDe04 <Bennettmason04@gmail.com>
2024-01-08 19:51:15 -0800

    Single Ip

--- os/server.nix
+++ os/server.nix
@@ -15,21 +15,7 @@
 
       virtualHosts."192.168.0.40" =  {
         locations."/" = {
-          proxyPass = "http://127.0.0.1:8384";
-          proxyWebsockets = true;
-        };
-      };
-
-      virtualHosts."192.168.0.10" =  {
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:8096";
-          proxyWebsockets = true;
-        };
-      };
-
-      virtualHosts."192.168.0.28" =  {
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:8080";
+          proxyPass = "http://127.0.0.1";
           proxyWebsockets = true;
         };
       };
@@ -40,10 +26,41 @@
       settings.PermitRootLogin = "yes";
     };
 
+    samba-wsdd = {
+      # make shares visible for windows 10 clients
+      enable = true;
+      openFirewall = true;
+    };
+
+    samba = {
+      enable = true; 
+
+      shares = {
+        Jellyfin = {
+          path = "/media";
+          "read only" = false;
+          browseable = "yes";
+          "guest ok" = "yes";
+          comment = "Jellyfin Media";
+        };
+
+        Gameyfin = {
+          path = "/var/lib/gameyfin";
+          # "read only" = true;
+          browseable = "yes";
+          "guest ok" = "yes";
+          comment = "Gameyfin Games";
+        };
+      };
+    };
+
     # Set: <insecureSkipHostcheck>true</insecureSkipHostcheck> in <gui>...</gui>
     # In: /var/lib/syncthing/.config/syncthing/config.xml
     # https://docs.syncthing.net/users/faq.html#why-do-i-get-host-check-error-in-the-gui-api
-    syncthing.enable = true;
+    syncthing = {
+      enable = true;
+      guiAddress = "192.168.0.40:8384";
+    };
 
     # Set: sudo setfacl -m u:jellyfin:rx /media/*
     # https://www.reddit.com/r/jellyfin/comments/gaojft/the_path_could_not_be_found_please_ensure_the/
